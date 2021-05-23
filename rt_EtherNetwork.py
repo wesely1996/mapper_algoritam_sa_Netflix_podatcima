3d72119f3a65d2591dc1db46239c961a82c82099
Nikola <nikola.wesely.96@gmail.com>
2021-05-23 19:34:28 +0200

    EtherNetwork class basics Just the inital values and start of the init funciton

---
+++ rt_EtherNetwork.py
@@ -0,0 +1,19 @@
+class EtherNetwork:
+
+	modelName = None
+	networkDF = None
+	origFrq = None
+	afterFrq = None
+	trimed = False
+	A = NA
+	B = NA
+	a = 1
+	b = 10
+	periodList = None
+	orgInfo = None
+	topInfo = None
+	tkOutFolder = None
+	
+	def __init__(modelName, fileLoc, timeFormat, outFolder):
+		self.modelName = modelName
+		#read data from data folder
