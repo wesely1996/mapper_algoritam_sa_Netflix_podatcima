5edabf2b162df977ade3e1c78f3989d209c44c20
Nikola <nikola.wesely.96@gmail.com>
2021-05-25 14:21:30 +0200

    dropExtreme and record func

--- rt_EtherNetwork.py
+++ rt_EtherNetwork.py
@@ -24,8 +24,33 @@ class EtherNetwork:
         self.networkDF.read_csv(fileLoc, sep=' ')
         # TODO - change networkDF.time to appropriate format
         self.origFrq = pd.crosstab(self.networkDF, 'time')
-        self.tkOutFolder = outFolder + '/' + modelName
+        self.tkOutFolder = outFolder + '\\' + modelName
         try:
             os.mkdir(self.tkOutFolder)
         except OSError():
             print("Creation of the directory %s failed" % self.tkOutFolder)
+
+    def dropExtreme(self, percentage=1 / 500):
+        if not self.trimmed:
+            rankDrop = len(self.networkDF) * percentage
+            self.A = self.networkDF.sort_values(by=['value'], ascending=True)[rankDrop]
+            self.A = self.networkDF.sort_values(by=['value'], ascending=False)[rankDrop]
+
+            self.networkDF = self.networkDF[self.networkDF['value'] > self.A &
+                                            self.networkDF['value'] < self.B]
+            self.afterFrq = pd.crosstab(self.networkDF, 'time')
+            self.trimmed = True
+        else:
+            print("The data set has already been trimmed.")
+
+    def record(self):
+        fileLoc = self.tkOutFolder + "\\dataSummary.txt"
+        file = open(fileLoc, "w+")
+
+        file.write("Original:\n")
+        file.write(self.origFrq)
+        file.write("Valid range: [" + self.A + ", " + self.B + "]\n")
+        file.write("Value summery:\n")
+        file.write(self.networkDF['value'].describe())
+
+        file.close()
