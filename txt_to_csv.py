a4c69f8bfcbc604c33395dad0d536372a9453851
Kosta Grujcic <kostag@microsoft.com>
2021-06-01 21:21:56 +0200

    Fixed txt_to_csv.

--- txt_to_csv.py
+++ txt_to_csv.py
@@ -1,13 +1,21 @@
-file_path_read = "./data/combined_data_all.txt"
-file_path_write = "./data/combined_data.csv"
+import os
 
-with open(file_path_read, 'r') as file:
-    with open(file_path_write, 'w+') as output:
-        movie_id = '0'
-        for line in file:
-            if line[:-2].isnumeric():
-                movie_id = line[:-2]
-            else:
-                output.write(movie_id + ',' + line)
-    output.close()
-file.close()
+
+if __name__ == "__main__":
+    data_dir = "./data"
+    merged_txt_file = os.path.join(data_dir, "combined_data_all.txt")
+    merged_csv_file = os.path.join(data_dir, "combined_data_all.csv")
+
+    movie_id = None
+    with open(merged_csv_file, "w") as csv_f:
+        with open(merged_txt_file, "r") as txt_f:
+            for line in txt_f:
+                line_split = line.split(",")
+
+                if len(line_split) == 1:
+                    movie_id = line.replace(":", "")
+                    movie_id = movie_id.replace("\n", "")
+                    movie_id = movie_id.replace("\r\n", "")
+                else:
+                    line = movie_id + "," + line
+                    csv_f.write(line)
