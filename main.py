c5fb9b21d7c845297bf2e9e4134435bcc989ace1
Nikola <nikola.wesely.96@gmail.com>
2021-06-03 02:30:19 +0200

    Data preperation

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ if __name__ == '__main__':
     data_file = os.path.join(data_dir, "combined_data_sample.csv")
 
     data = pd.read_csv(data_file)
+    data['MovieID'] = data['MovieID'].astype(int)
+    data['CustomerID'] = data['CustomerID'].astype(int)
+    data['Date'] = pd.to_datetime(data['Date'], format='%Y-%m-%d')
+
     train_data, test_data = train_test_split(data, test_size=0.2)
 
     x_train_data = train_data.loc[:, train_data.columns != 'Rating']
@@ -17,3 +21,8 @@ if __name__ == '__main__':
 
     x_test_data = test_data.loc[:, train_data.columns != 'Rating']
     y_test_data = test_data['Rating']
+
+    print('Uci se na ', x_train_data.shape[0], ' primera, a testira se na ', x_test_data.shape[0], ' primera')
+
+    y_train_data = (y_train_data >= 4).astype(int)
+    y_test_data = (y_test_data >= 4).astype(int)
