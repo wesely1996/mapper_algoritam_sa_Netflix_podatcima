f106c8c154566ed84f5009a0e7bca192f9baef8a
Nikola <nikola.wesely.96@gmail.com>
2021-06-03 13:23:02 +0200

    NN learning

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-import pandas as pd
-import numpy as np
 import os
+import io
 import base64
 
-# Tensorflow only supports Python 3.5 to 3.8. => change python interpreter to 3.8
-# import keras
-# from keras import backend as K
-# from keras.models import Sequential
-# from keras.layers import Dense, Dropout
-# from keras.optimizers import Adam
+import keras
+from keras import backend as k
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import Adam
+
+import kmapper as km
+import numpy as np
+import pandas as pd
+
+from matplotlib import pyplot as plt
 
 from sklearn.model_selection import train_test_split
+from sklearn import metrics, cluster
 
 if __name__ == '__main__':
     data_dir = "./data"
@@ -39,8 +44,54 @@ if __name__ == '__main__':
     y_test_data = (y_test_data >= 4).astype(int)
 
     # Model
-    heep_size = 128
+    heep_size = int(len(x_train_data.index) / 100)
     class_number = 1
     epoch_number = 10
 
-    # model = Sequential()
+    model = Sequential()
+    model.add(Dropout(0.5, input_shape=(3,)))
+    model.add(Dense(512, activation='relu', input_shape=(3,)))
+    model.add(Dropout(0.5))
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(class_number, activation='sigmoid'))
+
+    model.summary()
+    model.compile(loss='binary_crossentropy',
+                  optimizer=Adam(),
+                  metrics=['accuracy'])
+
+    history_of_learning = model.fix(x_train_data, y_train_data,
+                                    batch_size=heep_size,
+                                    epochs=epoch_number,
+                                    verbose=1,
+                                    validation_data=(x_test_data, y_test_data))
+    result = model.evaluate(x_test_data, y_test_data, verbose=0)
+    print("Greska: ", result[0], "\nTacnost: ", result[1])
+
+    number_of_passes = 1000
+
+    predicted_stochs = k.function(input=[model.layers[0].input,
+                                              k.learning_phase()],
+                                       output=[model.layers[-1].output])
+
+    y_pred_test = np.array([predicted_stochs([x_test_data, 1]) for _ in range(number_of_passes)])
+    y_pred_stochs_test = y_pred_test.reshape(-1, y_test_data.shape[0]).T
+
+    y_pred_std_test = np.std(y_pred_stochs_test, axis=1)
+    y_pred_mean_test = np.mean(y_pred_stochs_test, axis=1)
+    y_pred_modus_test = (np.mean(y_pred_stochs_test > .5, axis=1) > .5).astype(int).reshape(-1, 1)
+
+    y_pred_var_rel_test = 1 - np.mean((y_pred_stochs_test > .5) == y_pred_modus_test, axis=1)
+
+    analyse_test_results = pd.DataFrame({
+        "y_tacno": y_test_data,
+        "y_pred": y_pred_mean_test,
+        "VR": y_pred_var_rel_test,
+        "STD": y_pred_std_test
+    })
+
+    print("Tacnost:", metrics.accuracy_score(y_true=y_test_data, y_pred=y_pred_mean_test > .5))
+    print("Opis rezultata:")
+    print(analyse_test_results.describe())
+
